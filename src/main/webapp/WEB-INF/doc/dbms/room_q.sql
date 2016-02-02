b05b7647bd06e8f0cdbde06cc679a08fa6d1b52a
kimyoung3434 <kimyoung3434@naver.com>
2016-02-02 20:22:37 +0900

    ..

--- src/main/webapp/WEB-INF/doc/dbms/room_q.sql
+++ src/main/webapp/WEB-INF/doc/dbms/room_q.sql
@@ -11,7 +11,7 @@ CREATE TABLE room(
 		person_max                   		MEDIUMINT		  NOT NULL COMMENT '인원최대',
 		structure                     				VARCHAR(20)		  NOT NULL  COMMENT '구조',
 		room_type                      		VARCHAR(20)     NOT NULL COMMENT '타입',
-		equipment                      		VARCHAR(80)		  NOT NULL COMMENT '시설',
+		equipment                      		VARCHAR(100)		  NOT NULL COMMENT '시설',
 		money1                      		MEDIUMINT		  NOT NULL  COMMENT 'money1',
 		money2                      		MEDIUMINT		  NOT NULL COMMENT 'money2',
 		money3                      		MEDIUMINT		  NOT NULL  COMMENT 'money3',
@@ -24,20 +24,24 @@ CREATE TABLE room(
 
 2. 등록
 
-INSERT INTO room(roomno,room_name,person_min,person_max,structure,room_type,equipment,money1,
-										money2,money3,photo1,photo2,photo3,photo4,photo5)
-VALUES(1, '삶은감자',2,2,원룸형,가족,침대,70000,100000,100000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
+INSERT INTO room(roomno, room_name, person_min, person_max, structure, room_type,equipment,money1,money2,money3,photo1,photo2,photo3,photo4,photo5)
+VALUES(1, '삶은감자',2,2,'원룸형','가족','침대',70000,100000,100000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
+INSERT INTO room(roomno,room_name,person_min,person_max,structure,room_type,equipment,money1
+										,money2,money3,photo1,photo2,photo3,photo4,photo5)
+VALUES(2, '찐감자',4,6,'한실','커플',TV,160000,230000,230000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
 
-INSERT INTO room(roomno,room_name,person_min,person_max,structure,room_type,equipment,money1,
-										money2,money3,photo1,photo2,photo3,photo4,photo5)
-VALUES(2, '찐감자',4,6,한실,커플,TV,160000,230000,230000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
+INSERT INTO room(roomno,room_name,person_min,person_max,structure,room_type,equipment,money1
+										,money2,money3,photo1,photo2,photo3,photo4,photo5)
+VALUES(3, '생감자',6,8,'복층,단체','냉장고',260000,260000,260000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
 
-INSERT INTO room(roomno,room_name,person_min,person_max,structure,room_type,equipment,money1,
-										money2,money3,photo1,photo2,photo3,photo4,photo5)
-VALUES(3, '생감자',6,8,복층,단체,냉장고,260000,260000,260000,'spring.jpg','summer.jpg','autumn.jpg','winter.jpg','rain.jpg');
 
 SELECT * FROM blog;
 
+3.목록
+
+SELECT 
+FROM room
+ORDER BY roomno DESC;
 
 
 
